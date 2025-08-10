a7e03732efb39867f8232c99d7fb53c58b1360ca
Ben Belland <benbelland@gmail.com>
2025-08-10 14:21:04 -0700

    moves the godot head around the screen with rotation

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -5,9 +5,9 @@ use godot::classes::Node2D;
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
 struct Player {
-	speed: f64,
+	angular_speed: f32,
+	speed: f32,
 	position: Vector2,
-	delta: f32,
 	direction: f32,
 	rotation: f32,
 	base: Base<Sprite2D>
@@ -56,35 +56,49 @@ impl ISprite2D for Player {
 		godot_print!("Hello, world!"); //Prints to the godot console
 
 		Self {
-			speed: 400.0,
+			angular_speed: 3.14159,
+			speed: 200.0,
 			position: Vector2::ZERO,
-			delta: 0.0,
 			direction: 0.0,
 			rotation: 0.0,
 			base,
 		}
 	}
 	fn process(&mut self, delta: f32) {
-		self.delta = delta;
-	}
-
-	fn unhandled_input(&mut self, event: Gd<InputEvent>) {
+		self.rotation = self.base().get_rotation();
+		self.direction = 0.0;
+		let event = Input::singleton();
+		let mut moved = false;
+		let mut rot: f32 = self.rotation;
 		if event.is_action_pressed("ui_left") {
 			self.direction = -1.0;
+			moved = true;
+			self.rotation = (self.direction * self.angular_speed * delta) as f32;
+			rot = self.rotation;
 		}
 		if event.is_action_pressed("ui_right") {
 			self.direction = 1.0;
+			moved = true;
+			self.rotation = (self.direction * self.angular_speed * delta) as f32;
+			rot = self.rotation;
 		}
-		self.rotation += self.direction as f32;
-		let rot: f32 = self.rotation;
-		self.base_mut().rotate(rot);		
-		let mut velocity = Vector2::ZERO;
+		if moved {
+			self.base_mut().rotate(rot);
+			moved = false;
+		}
+		let rot = self.base().get_rotation();		
+//		self.position = Vector2::ZERO;
 		if event.is_action_pressed("ui_up") {
-			velocity = Vector2::UP.rotated(self.rotation) * self.speed as f32;
+			let velocity = Vector2::UP.rotated(rot) * self.speed as f32;
+			self.position = velocity * delta;
+			let pos: Vector2 = self.position;
+			let in_bounds = self.base().get_position();
+			if in_bounds.x <= 1100.0 && in_bounds.y <= 600.0 && in_bounds.x > 0.0 && in_bounds.y > 0.0{
+				self.base_mut().translate(pos);
+			}else {
+				godot_print!("Hit boundary!");
+			}
 		}
-		self.position += velocity * self.delta;
-		let pos: Vector2 = self.position;
-		self.base_mut().translate(pos);
 		//let radians = (self.angular_speed * delta) as f32;
 		//self.base_mut().rotate(radians);
 		// .rotate() requires a f32, so we convert it as a f32 rather than f64

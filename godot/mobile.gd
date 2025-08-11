6bfa35b73580abb8908dc21bc33cca707dc1297d
Ben Belland <benbelland@gmail.com>
2025-08-10 19:37:48 -0700

    Added three buttons

--- godot/mobile.gd
+++ godot/mobile.gd
@@ -4,3 +4,9 @@ func missile():
 func _process(_delta):
 	if Input.is_action_just_pressed("Pad-A"):
 		self.missile()
+	if Input.is_action_just_pressed("Pad-B"):
+		print("B")
+	if Input.is_action_just_pressed("Pad-X"):
+		print("X")
+	if Input.is_action_just_pressed("Pad-Y"):
+		print("Y")

4361cc09a03392e17802a9b831947b53afe05d54
Ben Belland <benbelland@gmail.com>
2025-08-17 15:10:17 -0700

    positioning the peeps in their places

--- godot/chef.gd
+++ godot/chef.gd
@@ -3,7 +3,10 @@ extends Mobiles
 
 func _on_balete() -> void:
 	self.queue_free()
-
+func _enter_tree() -> void:
+	var new_texture = preload("res://sprites/chef-normal.png")
+	self.texture = new_texture
+	self.position = Vector2(300.0, 200.0)
 
 func _on_sprite_2d_damage_all_mobiles(amount: int) -> void:
 	self.mobile_damage_emit(amount)

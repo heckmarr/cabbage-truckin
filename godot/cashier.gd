5849c55ac352e75ac70c3a9b9a184ec414d9899b
Ben Belland <benbelland@gmail.com>
2025-08-20 14:13:41 -0700

    It was so simple! I didn't need to cast because I already had a copy of it in the receiver

--- godot/cashier.gd
+++ godot/cashier.gd
@@ -5,8 +5,6 @@ func _on_balete() -> void:
 	self.queue_free()
 
 func _enter_tree() -> void:
-	var new_texture = preload("res://sprites/cashier-normal.png")
-	self.texture = new_texture
 	self.position = Vector2(200.0, 400.0)
 func _on_sprite_2d_damage_all_mobiles(amount: int) -> void:
 	self.mobile_damage_emit(amount)
--- godot/chef.gd
+++ godot/chef.gd
@@ -4,9 +4,7 @@ extends Mobiles
 func _on_balete() -> void:
 	self.queue_free()
 func _enter_tree() -> void:
-	var new_texture = preload("res://sprites/chef-normal.png")
-	self.texture = new_texture
-	self.position = Vector2(300.0, 200.0)
+	#self.position = Vector2(300.0, 200.0)
 	var format_string = "Chef is at %s"
 	var actual_string = format_string % self.position
 	print(actual_string)

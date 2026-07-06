26d82e25198a68e2b4e9ac1b85105ff3fb51895d
Ben Belland <benbelland@gmail.com>
2026-07-06 14:11:22 -0700

    More for the selected section

--- rust/src/game_state.rs
+++ rust/src/game_state.rs
@@ -28,6 +28,9 @@ impl GameState {
 impl INode2D for GameState {
 	fn init(base: Base<Node2D>) -> Self {
 		Self {
+			
+
+			//collections for selection and live checking
 			selected: HashMap::new(),
 			employees: HashMap::new(),
 			base,
@@ -49,10 +52,55 @@ impl INode2D for GameState {
 		self.selected.insert(MobileKind::Stocker, false);
 		self.selected.insert(MobileKind::WarehousePerson, false);
 
+		//Selected bounding rectangles
+		let selected_mob = MobileKind::Cashier;
 
 		for (employee_type, alive) in &self.employees {
 			match employee_type {
 				MobileKind::Cashier => {
+					//Todo Fill in with all other mob types
+
+					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
+					//cashier.signals().mob_die().connect_self(Self::die);
+				},
+				MobileKind::Package => {
+					//Todo Fill in with all other mob types
+
+					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
+					//cashier.signals().mob_die().connect_self(Self::die);
+				},
+				MobileKind::WarehousePerson => {
+					//Todo Fill in with all other mob types
+
+					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
+					//cashier.signals().mob_die().connect_self(Self::die);
+				},
+				MobileKind::Chef => {
+					//Todo Fill in with all other mob types
+
+					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
+					//cashier.signals().mob_die().connect_self(Self::die);
+				},
+				MobileKind::Stocker => {
+					//Todo Fill in with all other mob types
+
+					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
+					//cashier.signals().mob_die().connect_self(Self::die);
+				},
+				MobileKind::Customer => {
+					//Todo Fill in with all other mob types
+
+					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
+					//cashier.signals().mob_die().connect_self(Self::die);
+				},
+			}
+		}
+		for (employee_, selected) in &self.selected {
+			match employee_ {
+				MobileKind::Cashier => {
+					if *selected {
+						break;
+					}
 					//Todo Fill in with all other mob types
 
 					//let cashier = self.base().find_child("Cashier").expect("Cashier is already dead!");
